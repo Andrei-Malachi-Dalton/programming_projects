a4eb5d4925224e747faf3a9b9093c0323d6358e1
Andrei Dalton <adalton15@student.cscc.edu>
2022-01-12 13:38:13 -0500

    Update BMICalculator.php
    Finished product. This calculator calculates your BMI and prints it out.

--- BMICalculator.php
+++ BMICalculator.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
-<html lang="en>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BMI Calculator</title>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <style>
+        .error {
+            color: #6f0000;
+        }
+
+        .container {
+            padding: 10px;
+            margin-top: 40px;
+            background-color: #5EBF80;
+            border-radius: 10px;
+        }
+
+        .form {
+            margin: 0 auto;
+        }
+    </style>
 </head>
 
 <body>
@@ -56,8 +72,8 @@
     function calculateBMI($weight, $height){
         $meters = $height / 39.37;
         $kilos = $weight / 2.2046;
-
-        return $kilos / pow($meters, 2);
+        $bmi = $kilos / pow($meters, 2)
+        return number_format($bmi, 2, ".");
     }
 
     function classifyBMI($bmi) {
@@ -73,4 +89,32 @@
             return null;
         }
     }
+    ?>
+
+    <div class="container">
+        <h1 class="text-center">BMI Calculator</h1>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                    <input class="form-control" type="text" name="weight" id="weight" required>
+                    <label for="weight">Your weight</label>
+                    <span class="error">*<?php echo $weightErr; ?></span>
+                    </br>
+
+                    <input class="form-control" type="text" name="height" id="height" required>
+                    <label for="height">Your height in inches</label>
+                    <span class="error">* <?php echo $heightErr; ?></span>
+                    </br>
+                    <input class="btn btn-success" type="submit" name="calculate" value="Calculate">
+                </form>
+            </div>
+            <div class="col-12 col-md-6">
+                <h2 class="text-center">Your BMI</h2>
+
+                <p> <?php echo "Your BMI is $bmi."; ?>
+                <p> <?php echo "$bmiClassification"; ?>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
